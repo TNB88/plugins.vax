cf7c3c367b5c46e8fc1e823c746ecda779c6fd81
youngbi <32502366+youngbi@users.noreply.github.com>
2026-03-14 21:12:15 +0700

    Update missav2_plugin.js

--- plugins/missav2_plugin.js
+++ plugins/missav2_plugin.js
@@ -12,8 +12,7 @@ function getManifest() {
         "isEnabled": true,
         "isAdult": true,
         "type": "VIDEO",
-        "layoutType": "HORIZONTAL",
-        "subtitleCat": true
+        "layoutType": "HORIZONTAL"
     });
 }
 
